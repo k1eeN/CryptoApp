318eaf07578881c251012c7bc55785f8f5346511
Mark Klinitskii <mark23klin@mail.ru>
2025-12-29 23:30:36 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -0,0 +1,35 @@
+package com.example.cryptoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.adapters.CoinInfoAdapter
+import com.example.cryptoapp.pojo.CoinPriceInfo
+
+class CoinPriceListActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CoinViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_prce_list)
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromSymbol
+                )
+                startActivity(intent)
+            }
+        }
+        val rvCoinPriceList = findViewById<RecyclerView>(R.id.rvCoinPriceList)
+        rvCoinPriceList.adapter = adapter
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
+        })
+    }
+}
---
+++ app/src/main/java/com/example/cryptoapp/api/ApiService.kt
@@ -0,0 +1,34 @@
+package com.example.cryptoapp.api
+
+import com.example.cryptoapp.pojo.CoinInfoListOfData
+import com.example.cryptoapp.pojo.CoinPriceInfoRawData
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("top/totalvolfull")
+    fun getTopCoinsInfo(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "dae76a8505cff9ec7e47e1458cbbb5b5ffb9c392ef0c635747a4af6b2745e324",
+        @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
+        @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
+    ): Single<CoinInfoListOfData>
+
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "dae76a8505cff9ec7e47e1458cbbb5b5ffb9c392ef0c635747a4af6b2745e324",
+        @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
+        @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
+    ): Single<CoinPriceInfoRawData>
+
+    companion object {
+        private const val QUERY_PARAM_API_KEY = "api_key"
+        private const val QUERY_PARAM_LIMIT = "limit"
+        private const val QUERY_PARAM_TO_SYMBOL = "tsym"
+        private const val QUERY_PARAM_TO_SYMBOLS = "tsyms"
+        private const val QUERY_PARAM_FROM_SYMBOLS = "fsyms"
+
+        private const val CURRENCY = "USD"
+    }
+}
