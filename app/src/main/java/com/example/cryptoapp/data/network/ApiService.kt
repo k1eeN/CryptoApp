6ded03fecadc0e64c6908ace7401061071b2b8e9
Mark Klinitskii <mark23klin@mail.ru>
2025-12-31 18:07:25 +0300

    Lesson 2 - Create Mapper & Refactoring Code

--- app/src/main/java/com/example/cryptoapp/data/network/ApiService.kt
+++ app/src/main/java/com/example/cryptoapp/data/network/ApiService.kt
@@ -1,26 +1,25 @@
 package com.example.cryptoapp.data.network
 
-import com.example.cryptoapp.data.network.model.CoinNamesListDto
 import com.example.cryptoapp.data.network.model.CoinInfoJsonContainerDto
-import io.reactivex.Single
+import com.example.cryptoapp.data.network.model.CoinNamesListDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("top/totalvolfull")
-    fun getTopCoinsInfo(
+    suspend fun getTopCoinsInfo(
         @Query(QUERY_PARAM_API_KEY) apiKey: String = "dae76a8505cff9ec7e47e1458cbbb5b5ffb9c392ef0c635747a4af6b2745e324",
         @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
         @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
-    ): Single<CoinNamesListDto>
+    ): CoinNamesListDto
 
     @GET("pricemultifull")
-    fun getFullPriceList(
+    suspend fun getFullPriceList(
         @Query(QUERY_PARAM_API_KEY) apiKey: String = "dae76a8505cff9ec7e47e1458cbbb5b5ffb9c392ef0c635747a4af6b2745e324",
         @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
         @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
-    ): Single<CoinInfoJsonContainerDto>
+    ): CoinInfoJsonContainerDto
 
     companion object {
         private const val QUERY_PARAM_API_KEY = "api_key"
